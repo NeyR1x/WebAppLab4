69524547638a57095fc726c6f417a545adce3f16
NeyR1x <neyr1x.captain@mail.ru>
2022-04-02 23:02:54 +0300

    Initial commit

---
+++ MyWebApp/app.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)  # app - объект на основе класса Flask
+
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="171717",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+@app.route('/login/', methods=['GET'])    # http://127.0.0.1:5000/login
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND "
+                    "password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+    if records != []:
+        return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+    else:
+        return render_template('ff.html')
+if __name__ == "__main__":
+    app.run(debug=True)  # debug=True - ошибки выводятся на сайте
